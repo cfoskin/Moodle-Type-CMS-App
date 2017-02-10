70ecf14ef883d3f6a987a8726315b1a7133bea03
Colum Foskin <columfoskin@gmail.com>
2017-02-10 14:17:55 +0000

    updated home page & allow create module with no files

--- public/controllers/createModuleController.js
+++ public/controllers/createModuleController.js
@@ -3,36 +3,44 @@ var moodleApp = angular.module('moodleApp');
 moodleApp.controller('createModuleController',
     function($scope, $rootScope, $location, $http, moduleService, fileService, Upload, $timeout) {
         $scope.createModule = function(file) {
+
             var file = $scope.file;
-            file.upload = Upload.upload({
-                url: '/api/upload',
-                data: {
-                    file: file
-                }
-            });
-            file.upload
-                .then(function(response) {
-                    let newFile = {};
-                    $timeout(function() {
-                        file.result = response.data;
-                        fileService.postFile(file.result)
-                            .then(function(res) {
-                                newFile = res.data.file;
-                            })
-                            .then(function(res) {
-                                $scope.newModule.files = [];
-                                $scope.newModule.files.push(newFile);
-                                moduleService.postModule($scope.newModule)
-                                    .then(function(res) {
-                                        $location.path('/home');
-                                    })
-                            })
-                    });
-                }, function(response) {
-                    if (response.status > 0)
-                        $scope.errorMsg = response.status + ': ' + response.data;
-                }, function(evt) {
-                    file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+            if (file) {
+                file.upload = Upload.upload({
+                    url: '/api/upload',
+                    data: {
+                        file: file
+                    }
                 });
+                file.upload
+                    .then(function(response) {
+                        let newFile = {};
+                        $timeout(function() {
+                            file.result = response.data;
+                            fileService.postFile(file.result)
+                                .then(function(res) {
+                                    newFile = res.data.file;
+                                })
+                                .then(function(res) {
+                                    $scope.newModule.files = [];
+                                    $scope.newModule.files.push(newFile);
+                                    moduleService.postModule($scope.newModule)
+                                        .then(function(res) {
+                                            $location.path('/home');
+                                        })
+                                })
+                        });
+                    }, function(response) {
+                        if (response.status > 0)
+                            $scope.errorMsg = response.status + ': ' + response.data;
+                    }, function(evt) {
+                        file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+                    });
+            } else {
+                moduleService.postModule($scope.newModule)
+                    .then(function(res) {
+                        $location.path('/home');
+                    });
+            }
         }
     });
